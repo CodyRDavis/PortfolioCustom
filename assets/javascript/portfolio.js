4e1a4f63fbb3050213b03b8b440e9e27f375c097
Cody Davis <codydavis@codys-imac.lan>
2018-11-18 15:23:22 -0600

    linked portfolio to my github pages projects.

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,24 @@
+document.getElementById("projectOne").addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/");
+});
+document.getElementById("projectTwo").addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/Bootstrap-Portfolio/");
+});
+document.getElementById("projectThree").addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/guessLetter/");
+});
+document.getElementById("projectFour").addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/jQueryGame/");
+});
+document.getElementById("projectFive").addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/timerGame/");
+});
+document.getElementById("projectSix").addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/AJAXexample/");
+});
+document.getElementById("projectGOne").addEventListener('click', function(event){
+    window.open("https://jjmonte20.github.io/project1Team2/");
+});
+document.getElementById("appDiceBag").addEventListener('click', function(event){
+    window.open("https://codyrdavis.github.io/DiceBag/");
+});
