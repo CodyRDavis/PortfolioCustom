e11950903c40e48e65ba7989eac4d0a10295d379
Cody Davis <codydavis@codys-imac.lan>
2018-11-18 17:06:04 -0600

    brought contact me page online with styles and social media icons.

---
+++ assets/javascript/contact.js
@@ -0,0 +1,10 @@
+document.getElementById("button-twitter").addEventListener('click', function(event){
+    window.open("https://twitter.com/WizardSchnoz");
+
+});
+document.getElementById("button-linkedIn").addEventListener('click', function(event){
+    window.open("https://www.linkedin.com/in/cody-r-davis/");
+});
+document.getElementById("button-gitHub").addEventListener('click', function(event){
+    window.open("https://github.com/CodyRDavis");
+});
